2316f56047128418db9d7849c2eb8bc6b1f6eae5
Edmund Kawalec <e.kawalec@s4studio.pl>
2019-04-07 09:08:23 +0200

    warsztaty cz. 4 - czas

--- work/time.php
+++ work/time.php
@@ -19,7 +19,15 @@
     <input type="text" name="day">
     <input type="submit" name="sendButton" value="DALEJ">
 </form>
+<pre>
+<strong>Zadanie</strong>
+Odbierz dane urodzin użytkownika i wskaż jaki to był dzień tygodnia.
+</pre>
+
 <p>
+    <?php
+     // tu twój kod
+    ?>
 
 
 </p>
