85d2f77c118161df486244b2e6c006a6d17fcf59
Edmund Kawalec <e.kawalec@s4studio.pl>
2019-04-07 16:43:04 +0200

    warsztaty cz. 4 - wysyłamy pocztę przez php i swiftmailer'a

--- work/mail.php
+++ work/mail.php
@@ -6,3 +6,27 @@
  * Time: 16:28
  */
 
+// dopinam bibliotekę SwiftMailer - pobraną z GitHub
+require 'swiftmailer/swift_required.php';
+
+// tu twój kod php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP wysyła maile przez SMTP</title>
+</head>
+<body>
+<form action="">
+    <input type="text" name="topic" id=""><br>
+    <input type="text" name="recipient" id=""><br>
+    <textarea name="message" id="" cols="30" rows="10"></textarea>
+    <input type="submit" name="sendMessage" id="" value="Wyślij">
+</form>
+</body>
+</html>
+
