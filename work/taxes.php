15c81e9199cab725b3417231861c706959c3c47f
Edmund Kawalec <e.kawalec@s4studio.pl>
2019-04-07 12:29:48 +0200

    warsztaty cz. 4 - podatki

--- work/taxes.php
+++ work/taxes.php
@@ -20,16 +20,18 @@ Policz cenę brutto od podanej w formularzu. Stawka VAT: 23%.
 
     // tu twój kod
     $vat = 23;
-    $netto = (float)$_GET['netto'];
-    $brutto = $netto * (1 + $vat/100);
-    echo 'wartość netto: '. $netto;
-    echo '<br>';
-    echo 'wartość brutto: '. $brutto;
-    echo '<br>';
-    echo 'wartość brutto [via round()]: '. round($brutto, 2);
-    echo '<br>';
-    echo 'wartość brutto [via number_format()]: '. number_format($brutto, 2);
 
+    if (isset($_GET['netto'])) {
+        $netto = (float)$_GET['netto'];
+        $brutto = $netto * (1 + $vat/100);
+        echo 'wartość netto: '. $netto;
+        echo '<br>';
+        echo 'wartość brutto: '. $brutto;
+        echo '<br>';
+        echo 'wartość brutto [via round()]: '. round($brutto, 2);
+        echo '<br>';
+        echo 'wartość brutto [via number_format()]: '. number_format($brutto, 2);
+    }
 
     ?>
 </p>
@@ -46,9 +48,40 @@ Policz cenę netto od podanej brutto w formularzu. Stawka VAT: 23%.
 </form>
 <p>
     <?php
-        // tu kod :)
+        if (isset($_GET['brutto'])) {
+            // tu kod :)
+            $brutto = (float)$_GET['brutto'];
+            $netto = $brutto / (1 + $vat/100);
+            echo '<br>';
+            echo 'wartość netto: '. round($netto, 2);
+        }
+
     ?>
 </p>
 
+<hr>
+
+<pre>
+<strong>Zadanie:</strong>
+Pobierz aktualną tabelę kursów walut z NBP.
+</pre>
+<form action="">
+    <input type="submit" name="getCurrency" value="Pobierz" >
+</form>
+<p>
+    <?php
+    if (isset($_GET['getCurrency']) && $_GET['getCurrency']=='Pobierz') {
+        $table = 'a';
+        $url = 'http://api.nbp.pl/api/exchangerates/tables/'.$table;
+        echo $url;
+        echo '<br>';
+        $data = file_get_contents($url);
+        echo '<br>';
+        echo 'Format JSON: <br>';
+        echo $data;
+    }
+
+    ?>
+</p>
 </body>
 </html>
