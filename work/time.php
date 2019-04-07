d195c3e6a920e47d50b692c0ccfaa818fe04d4cb
Edmund Kawalec <e.kawalec@s4studio.pl>
2019-04-07 09:06:28 +0200

    warsztaty cz. 4 - czas

---
+++ work/time.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jaszczomp
+ * Date: 2019.04.07
+ * Time: 09:04
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="">
+    <input type="text" name="year">
+    <input type="text" name="month">
+    <input type="text" name="day">
+    <input type="submit" name="sendButton" value="DALEJ">
+</form>
+<p>
+
+
+</p>
+</body>
+</html>
