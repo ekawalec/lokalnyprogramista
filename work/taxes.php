c78f7f66f4e5b3833bbf95ef58ae1bbc5e94e6b6
Edmund Kawalec <e.kawalec@s4studio.pl>
2019-04-07 11:46:15 +0200

    warsztaty cz. 4 - podatki

--- work/taxes.php
+++ work/taxes.php
@@ -8,7 +8,7 @@
 
 
 <form action="">
-    <input type="text" name="netto">
+    <input type="number" placeholder="kwota netto" name="netto" required step="0.01" min="0.01" max="1000000">
     <button type="submit">DALEJ</button>
 </form>
 <pre>
@@ -19,7 +19,33 @@ Policz cenę brutto od podanej w formularzu. Stawka VAT: 23%.
     <?php
 
     // tu twój kod
+    $vat = 23;
+    $netto = (float)$_GET['netto'];
+    $brutto = $netto * (1 + $vat/100);
+    echo 'wartość netto: '. $netto;
+    echo '<br>';
+    echo 'wartość brutto: '. $brutto;
+    echo '<br>';
+    echo 'wartość brutto [via round()]: '. round($brutto, 2);
+    echo '<br>';
+    echo 'wartość brutto [via number_format()]: '. number_format($brutto, 2);
 
+
+    ?>
+</p>
+
+
+<form action="">
+    <input type="number" placeholder="kwota brutto" name="brutto" required step="0.01" min="0.01" max="1000000">
+    <button type="submit">DALEJ</button>
+</form>
+<pre>
+<strong>Zadanie:</strong>
+Policz cenę netto od podanej brutto w formularzu. Stawka VAT: 23%.
+</pre>
+<p>
+    <?php
+        // tu kod :)
     ?>
 </p>
 
